040791b10465b0d3655672dadc7e55b023b92c6c
mihhran <mihranaslanyan2004@gmail.com>
2021-11-08 22:07:56 +0400

    Api

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-
+Route::middleware('auth:api')->get('/user',function (Request $request){
+    return $request->user();
+});
