992a777376c43c6e67b219fad39e60a8f0a31127
mihhran <mihranaslanyan2004@gmail.com>
2021-11-12 22:15:47 +0400

    postman

--- database/migrations/2021_10_29_173429_add_user_id_to_products_table.php
+++ database/migrations/2021_10_29_173429_add_user_id_to_products_table.php
@@ -15,6 +15,11 @@ class AddUserIdToProductsTable extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')->after('price');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
@@ -25,6 +30,9 @@ class AddUserIdToProductsTable extends Migration
      */
     public function down()
     {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+        });
         Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('user_id');
         });
